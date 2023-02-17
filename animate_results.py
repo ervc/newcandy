80237554db4ae39c12ac2d428cb7cf1d34a685f1
Eric Van Clepper <ericvc@uchicago.edu>
2023-02-17 10:51:03 -0600

    included comments for plotting scripts

--- animate_results.py
+++ animate_results.py
@@ -1,38 +1,71 @@
+# standard imports
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
+# local imports
 import chemdiff as cd
 
+# candy output file
 candyfile = 'growth_example/output_abuns.npz'
+# the name of the gif we will be making
 anifile = 'growth_example/animation.gif'
 
+# load candy results
 cdout = np.load(candyfile)
 
+# which species do we want to plot
 plotspecs = ['CO','grainCO','H2O','grainH2O','grain']
+
+# candy ouputs
 specs = cdout['species']
 abuns = cdout['abundances']
 times = cdout['times']
 
+# some helpful physical parameters
 r = 30*cd.constants.AU
 h = cd.disk.get_scaleheight(r)
 
+# create the figure
 fig,ax = plt.subplots()
+# create an empty dictionary to store the lines that are plotted
+# the dictionary will have entries so that the line for H2O
+# can be indexed using line[H2O]
 lines = {}
+# loop through, get the abundances for the species we want to plot
+# then plot the initial abundances and store the line created
+# in the dictionary
 for spec in plotspecs:
     if spec in specs:
+        # find which species index corresponds with the species of interest
         k = np.where(specs==spec)[0][0]
+        # plot abundance at time 0, all heights, species k vs Z/H
+        # ax.plot returns a tuple, but we only care about the line object,
+        # so that's why I say line, = ...
         line, = ax.plot(abuns[0,:,k],cdout['heights']/h,label=spec)
         lines[spec] = line
+# axes labels, title and legend
 ax.set(xscale='log',xlabel='abun',ylabel='Z/H')
 ax.legend()
 title = ax.set_title(f'{times[0]:.0f} yr')
 
+# the animate function is called when the animation is made, where the
+# argument i is the frame number
 def animate(i):
+    # loop through the species and lines from the lines dictionary
     for spec,line in lines.items():
+        # Again, get the right index
         k = np.where(specs==spec)[0][0]
+        # adjust the x data (since I'm plotting height as y, which doesn't change)
+        # the new x data is abundance at time i, all heights, species index k
         line.set_xdata(abuns[i,:,k])
+    # adjust the title
     title.set_text(f'{times[i]:.0f} yr')
+    # Technically you're supposed to return the objects you modify, I think
+    # that helps it run faster, but it works fine if you just don't return
+    # anything here
 
+# create and save the animation, I'm passing frames=len(times) to assure
+# we loop through all the times from the output
 anim = FuncAnimation(fig,animate,frames=len(times))
 anim.save(anifile)
--- plot_results.py
+++ plot_results.py
@@ -1,22 +1,42 @@
+# import (mostly) standard libraries
 import numpy as np
 import matplotlib.pyplot as plt
 
+# import chemdiff locally
 import chemdiff as cd
 
+# get the names of the CANDY output file and the image we want to make
 cdfile = 'growth_example/output_abuns.npz'
 pngfile = 'growth_example/results.png'
 
-
+# load the output file into NpzFile class
 cdout = np.load(cdfile)
 
-plotspecs = ['CO','grainCO','H2O','grainH2O','grain']
-specs = cdout['species']
-abuns = cdout['abundances']
-times = cdout['times']
+# we can get the outputs that we care about by indexing 
+# like a python dictionary.
+# You can see what is indices are available by printing
+### in : print(cdout.files)
+### out: ['times', 'species', 'abundances', 'heights', 'density', 'extinctions']
+specs = cdout['species']    # list of all species in the output file
+abuns = cdout['abundances'] # array of all the abundances of each species
+times = cdout['times']      # list of all the output times
+# abundances has shape (nt,nz,nspec)
+# species has shape (nspec)
+# times has shape (nt)
+# heights has shape (nz)
+# density has shape (nz)
+# extinctions has shape (nt,nz)
 
+# A list of species we want to plot
+plotspecs = ['CO','grainCO','H2O','grainH2O','grain']
+
+# some useful constants to have for plotting
 r = 30*cd.constants.AU
+# here we're using one of the helpful functions from chemdiff.disk
+# these disk parameters can be changed by editing funcitons in chemdiff/disk.py
 h = cd.disk.get_scaleheight(r)
 
+# create a figure and axis dictionary using handy subplot_mosaic function
 fig,axd = plt.subplot_mosaic(
     """
     ppaa
@@ -24,26 +44,46 @@ fig,axd = plt.subplot_mosaic(
     ppmm
     """, tight_layout=True
 )
+# Here, I want to plot the profile of a few species of interest at the end of
+# simulation.
+
+# for each species we want to plot
 for spec in plotspecs:
+    # if that species is included in the chemdiff output
     if spec in specs:
+        # get the index of the species
         k = np.where(specs==spec)[0][0]
+        # plot the output for the last time, all heights, species of interest
+        # abundances are indexed as abuns[nt,nz,nspec]
         axd['p'].plot(abuns[-1,:,k],cdout['heights']/h,label=spec)
+# format the axes
 axd['p'].set(xscale='log',xlabel='abun',ylabel='Z/H')
 axd['p'].legend()
 title = axd['p'].set_title(f'{times[-1]:.0f} yr')
 
+# Here I want to plot the time evolution of the midplane, warm molecular region,
+# and atmosphere of the disk. 
+# First I define the three regions of the disk (arbitrarily) as the lowest cell,
+# a cell half way up the column, and the cell at the very top
 cells = {
     'm' : 0,
     'w' : len(cdout['heights'])//2,
     'a' : len(cdout['heights'])-1,
 }
 
+# loop through each of the three cells defined above
 for label,cell in cells.items():
+    # select which axis we want to draw on
     ax = axd[label]
+    # for each species
     for spec in plotspecs:
+        # if that species exists
         if spec in specs:
+            # get the index
             k = np.where(specs==spec)[0][0]
+            # plot all times, the appropriate height, and the right species
             ax.plot(times,abuns[:,cell,k],label=spec)
+    # format each axis
     ax.set(xscale='log',ylabel='abun',yscale='log')
     ax.set(title=f'z/h = {cdout["heights"][cell]/h:.2f}')
     if label=='m':
@@ -51,4 +91,5 @@ for label,cell in cells.items():
     else:
         ax.set(xticks=[],xticklabels=[])
 
+# save the output!
 plt.savefig(pngfile)
