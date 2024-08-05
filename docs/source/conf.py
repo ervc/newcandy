9f37ca8add5493988464c21411ee45cc098a67a4
Eric Van Clepper <ericvc@uchicago.edu>
2024-08-05 16:08:35 -0500

    comment out theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,6 +32,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = ['_themes']
-html_static_path = ['_static']
+# html_theme = 'sphinx_rtd_theme'
+# html_theme_path = ['_themes']
+# html_static_path = ['_static']
